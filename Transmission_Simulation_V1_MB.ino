386ddb33116764442a81522872fd2a30c8b7c316
SomethingGeorge <81539071+SomethingGeorge@users.noreply.github.com>
2021-04-11 17:11:48 -0400

    Updated to have logic for all four Buttons
    All functions working except Downshift and Reverse.

--- Transmission_Simulation_V1_MB.ino
+++ Transmission_Simulation_V1_MB.ino
@@ -15,7 +15,7 @@ void setup() {
        pinMode(firstButton,INPUT);  // Park Button
        pinMode(secondButton,INPUT); // Forward and First Gear (If Park is Enabled and This is Pressed Forward in First)
        pinMode(thirdButton,INPUT);  // Second Gear (Will not Engage if Park or Reverse is Active)
-       pinMode(fourthButton,INPUT); // Geardown (Only if Pressed when Second Gear is Active) and Reverse (If Park is Enabled and This is Pressed Reverse)
+       pinMode(fourthButton,INPUT); // Geardown (Only if Pressed when Second Gear is Active); Reverse (If Park is Enabled and This is Pressed Reverse)
        pinMode(firstLED,OUTPUT);
        pinMode(secondLED,OUTPUT);
        pinMode(thirdLED,OUTPUT);
@@ -25,21 +25,43 @@ void setup() {
 
 void loop() {
  
-    // Park Enable, when Park Button is Pressed (firstButton) and Fourth Pushed
+      // Park Enable, when Park Button is Pressed (firstButton) and Fourth button is not Pressed 
       if (digitalRead(firstButton)==HIGH && digitalRead(fourthLED)==LOW) {
-            digitalWrite(firstLED,LOW);   
+            digitalWrite(firstLED,HIGH);   // Park is Engaged
+            digitalWrite(secondLED,LOW);  // First Gear is Disengaged 
+            digitalWrite(thirdLED,LOW);   // Second Gear is Disengaged 
       }  
+
+      // Reverse Enable, Park Gear is Disengaged and Reverse Gear is Engaged
+      if (digitalRead(firstButton)==HIGH && digitalRead(fourthLED)==HIGH){
+            digitalWrite(firstLED,LOW);   // Park is Disengaged
+            digitalWrite(fourthLED,HIGH); // Reverse Gear is Engaged      
+      } 
+        
       
-      
-     // 
-      if (digitalRead(secondButton)==HIGH){
-            digitalWrite(secondLED,HIGH);
-            digitalWrite(firstLED,LOW);
-      }   if (digitalRead(thirdButton)==HIGH){
-            digitalWrite(thirdLED,HIGH); 
-      }   if (digitalRead(fourthButton)==HIGH){
-            digitalWrite(fourthLED,HIGH); 
-            }
+     // First Gear is Engaged and Park is Disengaged
+      if (digitalRead(secondButton)==HIGH && digitalRead(firstLED)==HIGH){
+            digitalWrite(firstLED,LOW);   // Park is Disengaged
+            digitalWrite(secondLED,HIGH); // First Gear is Engaged      
+      } 
+
+
+     // Second Gear is Engaged and First Gear is Disengaged
+      if (digitalRead(thirdButton)==HIGH && digitalRead(secondLED)==HIGH){
+            digitalWrite(secondLED,LOW);   // First Gear is Disengaged
+            digitalWrite(thirdLED,HIGH); // Second Gear is Engaged      
+      } 
+
+   // Downshift Enable, Second Gear is Disengaged and First Gear is Engaged
+      if (digitalRead(thirdButton)==HIGH && digitalRead(secondLED)==LOW && digitalRead(fourthLED)==HIGH){
+            digitalWrite(thirdLED,LOW);   // Second Gear is Disengaged
+            digitalWrite(fourthLED,HIGH); // Downshift Active
+            digitalWrite(secondLED,HIGH); // first Gear is Engaged
+            delay(3000);
+            digitalWrite(fourthLED,LOW); // Downshift LED turned off after 3 seconds (Downshift Complete)    
+      } 
+        
+            
             Serial.println(digitalRead(firstButton));
             //digitalWrite(firstLED,LOW);
             //digitalWrite(secondLED,LOW);
